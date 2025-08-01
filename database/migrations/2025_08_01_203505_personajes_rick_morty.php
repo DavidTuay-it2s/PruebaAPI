5f5aed2d80f84684dbab8929070c9f0c00cb0007
DavidTuay-it2s <david@it2s.com.co>
2025-08-01 15:47:42 -0500

    creacion de tabla para api

---
+++ database/migrations/2025_08_01_203505_personajes_rick_morty.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        schema::create('personajes_rick_morty', function (Blueprint $table) {
+           $table->id();
+           $table->string('nombre');
+           $table->string('especimen');
+           $table->string('genero');
+           $table->string('imagen');
+           $table->string('estado');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
